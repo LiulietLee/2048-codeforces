5049a9c79129c6bb149da308dc4fa9a34173386e
LiulietLee <lee.liuliet@hotmail.com>
2018-05-31 16:32:21 +0800

    init

---
+++ 2048-cf/2048-cf/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2048-cf
+//
+//  Created by Liuliet.Lee on 31/5/2018.
+//  Copyright © 2018 Liuliet.Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
